54c15fb1b556d42e092f2c13447ce5b9d7515bd9
Milanpreetsingh <smilanpreet7@gmail.com>
2020-10-09 16:12:00 +0530

    Updates

---
+++ precache-manifest.b7bfce91e08b7498c560d3b140199621.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1ea20e03311b573803013bac7ece43",
+    "url": "/Infotech/index.html"
+  },
+  {
+    "revision": "abe58fadd59287671442",
+    "url": "/Infotech/static/css/main.4efed1d2.chunk.css"
+  },
+  {
+    "revision": "025c838425355b7abad0",
+    "url": "/Infotech/static/js/2.daa5193f.chunk.js"
+  },
+  {
+    "revision": "fcbf42754ab0b66528572837cd3c01e5",
+    "url": "/Infotech/static/js/2.daa5193f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe58fadd59287671442",
+    "url": "/Infotech/static/js/main.03b69334.chunk.js"
+  },
+  {
+    "revision": "0e649525a3dd4e1b883a",
+    "url": "/Infotech/static/js/runtime-main.1a9585d8.js"
+  },
+  {
+    "revision": "c58cb726492712a30788b93afbe48bd8",
+    "url": "/Infotech/static/media/pic1.c58cb726.png"
+  },
+  {
+    "revision": "126b93a3ed6b2b40624b03c2723b541b",
+    "url": "/Infotech/static/media/pic2.126b93a3.png"
+  },
+  {
+    "revision": "7c14d8ea648853a86d538e43b3d088b0",
+    "url": "/Infotech/static/media/pic3.7c14d8ea.png"
+  },
+  {
+    "revision": "3dc34a8d30d0493212247adc4860e70f",
+    "url": "/Infotech/static/media/pic_italian1.3dc34a8d.png"
+  },
+  {
+    "revision": "4051079f91e5cdda3212d6007370c551",
+    "url": "/Infotech/static/media/pic_italian2.4051079f.png"
+  },
+  {
+    "revision": "5b247201642dc747010596200798fd28",
+    "url": "/Infotech/static/media/pic_italian3.5b247201.png"
+  }
+]);
